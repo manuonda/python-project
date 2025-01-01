c1da7629d0fe16a3f5896791e6c0b3b36493aa8c
david garcia <david.garcia@grupomost.com>
2025-01-01 10:59:18 -0300

    remove unused files and example scripts from fine-tuning and swarm project structure

--- fine-tunning/Readme.md
+++
@@ -1,5 +0,0 @@
-#### Install 
-
-virtualenv env
-source env/bin/activate
-pip install unsloth==2024.11.11
--- py-profesional/functions.py
+++ py-profesional/functions.py
@@ -61,9 +61,109 @@ suma = lambda *args: sum(args)
 print(suma(1,2,3,4,5,6))
 
 def handle_operation(callback, *args, **kwargs):
-    return callback(args,kwargs)
+    print("function de orden superior")
+    result = callback(*args,**kwargs)
+    print(result)
 
 
-function = 
+def add(a,b):
+    return a+b
+
+handle_operation(add,5,3)
 
 
+def factory_operation(option):
+    def deposit(balance, amount=0):
+        return balance + amount
+    
+    def withdraw(balance, amount=0):
+        if amount > balance:
+            return None
+        
+        return balance - amount
+
+    default = lambda *args, **kwargs: '>>> Lo sentimos, opcion No valida'
+
+    if option == 'deposit':
+        return deposit
+    elif option == 'withdraw':
+         return withdraw 
+    else:
+        return default
+    
+option = input('Ingresa una opcion')
+func = factory_operation(option)
+
+print(func(100,20))
+print(type(func))
+
+#Closures
+def multiply(number1):
+    def operation(number2):
+        return number1 * number2
+    return operation
+    
+func_operation = multiply(10)
+print('El resultado es: ')
+result = func_operation(3)
+print(result)
+
+def my_decorator(func):
+    def wrapper(*args,**kwargs):
+        print("Algo se esta haciendo antes de la funcion")
+        result = func(*args,**kwargs)
+        print("Algo se esta haciendo despues de la funcion")
+        return result
+    return wrapper
+
+
+@my_decorator
+def say_hello():
+    print("Hola mundo!")
+
+say_hello()
+
+
+@my_decorator
+def suma(numer1, number2):
+    return numer1 + number2
+
+print(suma(10,20))
+
+
+def full_name(first_name, last_name):
+    """ Permite generar un nombre completo 
+        Args:
+           - first_name (String)
+           - last_name  (String)
+
+        Return:
+           String
+    """
+    return f"{first_name}{last_name}"
+
+print(
+    full_name.__doc__
+)
+
+
+def palindromo(sentence):
+    """ Permite conocer si un String es o no un palindromo
+     
+       Args:
+        - sentence (String) 
+
+        Return 
+         - bool
+
+        Examples:
+        >>> palindromo('oso')
+        True
+
+        >>> palindromo('hello')
+        False
+
+    """
+
+    sentence = sentence.lower().replace(' ','')
+    return sentence == sentence[::-1]
