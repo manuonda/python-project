740cd649ca48854ca1748e8d18e04bc04985b10b
david garcia <david.garcia@grupomost.com>
2025-01-14 00:10:34 -0300

    add state graph implementation with defined nodes and edges

--- agents/langraph/tutorial/main.py
+++ agents/langraph/tutorial/main.py
@@ -1,6 +1,41 @@
-from typing import Annotated
-from typing_extensions import TypedDict
-from langgraph.graph import Graph
-from langgraph.graph.message import add_messages
+from typing import TypedDict
+from langgraph.graph import StateGraph, START, END
+
+ # Define the state of the agent
+class State(TypedDict):
+    my_var: str
+    customer_nam: str
+
+# Define the nodes
+def node_1(state: State) -> State:
+    state['my_var'] = "Hello"
+    state['customer_nam']  = "John"  
+    return state
+
+def node_2(state: State) -> State:
+    customer_name = state['customer_name'] 
+    state['my_var'] = f"Hello {customer_name}" 
+    return state
+
+def node_3(state: State) -> State:
+    return state 
+
+# Define the graph 
+
+grap = StateGraph(State)
+
+grap.add_node('node_1', node_1)
+grap.add_node('node_2', node_2)
+grap.add_node('node_3', node_3)
+
+# define the edge
+grap.add_edge(START, 'node_1')
+grap.add_edge('node_1', 'node_2')
+grap.add_edge('node_2', 'node_3')
+grap.add_edge('node_3', END)
+
+grap.compile()
+
+
 
 
